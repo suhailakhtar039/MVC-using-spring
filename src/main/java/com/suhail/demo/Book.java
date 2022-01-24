588b567edc2f66ac570bd52d15e8808db207a1e1
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-01-24 23:30:41 +0530

    Created book class

---
+++ src/main/java/com/suhail/demo/Book.java
@@ -0,0 +1,28 @@
+package com.suhail.demo;
+
+public class Book {
+	int id;
+	String name;
+	String author;
+	
+	public Book(int id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+	
+}
