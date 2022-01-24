f320e61a7d5f1beef816f3cc54d751f3eb6b7d42
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-01-25 00:29:56 +0530

    Modified Book controller

--- src/main/java/com/suhail/demo/Book.java
+++ src/main/java/com/suhail/demo/Book.java
@@ -1,17 +1,17 @@
 package com.suhail.demo;
 
 public class Book {
-	int id;
+	long id;
 	String name;
 	String author;
 	
-	public Book(int id, String name, String author) {
+	public Book(long id, String name, String author) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.author = author;
 	}
-	public int getId() {
+	public long getId() {
 		return id;
 	}
 	public String getName() {
--- src/main/java/com/suhail/demo/BookController.java
+++ src/main/java/com/suhail/demo/BookController.java
@@ -1,8 +1,15 @@
 package com.suhail.demo;
 
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Arrays;
+import java.util.List;
 
-@ResponseBody
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookController {
-
+	@GetMapping("/books")
+	public List<Book> getAllBooks(){
+		return Arrays.asList(new Book(1l,"panchtantra","munsi prem chand"));
+	}
 }
